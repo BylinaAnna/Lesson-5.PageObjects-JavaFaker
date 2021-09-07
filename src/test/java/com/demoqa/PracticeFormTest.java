776f8eef36bde868ce0ce7205afaa552914f357d
BylinaAnna <bylina.anna.v@gmail.com>
2021-09-07 19:36:16 +0200

    Add homework

--- src/test/java/com/demoqa/PracticeFormTest.java
+++ src/test/java/com/demoqa/PracticeFormTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.text;
 
 
 
@@ -36,6 +37,22 @@ public class PracticeFormTest {
         $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").selectOption("1984");
         $x("//div[contains(@aria-label, \"January 5th, 1984\")]").click();
+        $("#subjectsInput").setValue("English").pressEnter();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("img/test.png");
+        $("#currentAddress").setValue("Lunacharskogo,10");
+        $("#currentAddress").scrollIntoView(true);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Anna Petrova"),text("chaika@gmail.com"),text("Female"),
+                text("7903627451"),text("05 January,1984"),text("English, Maths"),text("Music"),text("test.png"),
+                text("test.png"),text("Lunacharskogo,10"),text("NCR Delhi"));
+
 
 
 
