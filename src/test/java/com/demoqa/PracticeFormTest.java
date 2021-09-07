414e0bb99ea1769d54e58df5c36765e5f83910e4
BylinaAnna <bylina.anna.v@gmail.com>
2021-09-07 14:23:36 +0200

    New code

--- src/test/java/com/demoqa/PracticeFormTest.java
+++ src/test/java/com/demoqa/PracticeFormTest.java
@@ -1,21 +1,27 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.files.DownloadActions.click;
+
+
 
 
 public class PracticeFormTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.startMaximized = true;
+        Configuration.holdBrowserOpen = true;
     }
-
+    @AfterAll
+    static void afterAll() {
+        Configuration.holdBrowserOpen = true;
+    }
     @Test
     void fillFormTest() {
         open("https://demoqa.com/automation-practice-form");
@@ -23,7 +29,16 @@ public class PracticeFormTest {
         $("#lastName").setValue("Petrova");
         $("#userEmail").setValue("chaika@gmail.com");
         $("#genterWrapper").$(byText("Female")).click();
-        //$("#lastName").setValue("Petrova");
+        $("#userNumber").setValue("7903627451");
+        $("#userNumber").setValue("7903627451");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("1984");
+        $x("//div[contains(@aria-label, \"January 5th, 1984\")]").click();
+
+
+
 
 }
 }
